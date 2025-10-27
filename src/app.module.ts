a0c1975894c6a100e83933b4f5eefa1c590409a3
LeonardoKako <fleithkako@gmail.com>
2025-10-27 13:42:35 -0300

    connection to Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      database: 'postgres',
+      password: 'leo',
+      autoLoadEntities: true, // carrega entidades sem precisar especifica-las
+      synchronize: true, // sincroniza com o BD. Não deve ser usado em produção
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
