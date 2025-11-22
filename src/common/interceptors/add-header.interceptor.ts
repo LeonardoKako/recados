dfb34fb500e7365142bb7cceb0a12eb2ccb30df9
LeonardoKako <fleithkako@gmail.com>
2025-11-22 15:50:06 -0300

    interceptors

---
+++ src/common/interceptors/add-header.interceptor.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+export class AddHeaderInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const response = context.switchToHttp().getResponse();
+    response.setHeader('X-Custom-Header', 'O valor do cabeçalho');
+    return next.handle();
+  }
+}
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -7,15 +7,16 @@ import {
   Patch,
   Post,
   Query,
-  UsePipes,
+  UseInterceptors,
 } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { ParseIntIdPipe } from 'src/common/pipes/parse-int-pipe';
+import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interceptor';
 
 @Controller('messages')
+@UseInterceptors(AddHeaderInterceptor)
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
